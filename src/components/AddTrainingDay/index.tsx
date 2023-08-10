4ecb3a761cd0585c9abf86da776049440ef72d60
PavlenkoSt <verbatim8872@gmail.com>
2023-08-10 22:20:08 +0300

    configured absolute imports

--- src/components/AddTrainingDay/index.tsx
+++ src/components/AddTrainingDay/index.tsx
@@ -1,6 +1,11 @@
-import React, {memo} from 'react';
+import React, {memo, useState} from 'react';
 import {Text, View} from 'react-native';
+
+import {IDateExercises} from '@/types/IDateExercises';
+
 const AddTrainingDay = () => {
+  const [exercises, setExercises] = useState<IDateExercises[]>([]);
+
   return (
     <View>
       <Text>AddTrainingDay</Text>
