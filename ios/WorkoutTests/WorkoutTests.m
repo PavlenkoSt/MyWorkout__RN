8b91855ef41bb25ecf77d6a399a4b1cd2fdbdf6e
PavlenkoSt <verbatim8872@gmail.com>
2023-08-12 23:09:38 +0300

    rename project

--- ios/WorkoutTests/WorkoutTests.m
+++ ios/WorkoutTests/WorkoutTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MyWorkoutTests : XCTestCase
+@interface WorkoutTests : XCTestCase
 
 @end
 
-@implementation MyWorkoutTests
+@implementation WorkoutTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
