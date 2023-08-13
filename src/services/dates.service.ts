90958d0f72e337d4aa3fabdb624098986786833f
PavlenkoSt <verbatim8872@gmail.com>
2023-08-13 10:56:58 +0300

    fix isPassedDate

--- src/services/dates.service.ts
+++ src/services/dates.service.ts
@@ -1,7 +1,7 @@
 class DatesService {
   today = new Date().toISOString().split('T')[0];
 
-  isPassedDate = (date: Date) => new Date(this.today) >= date;
+  isPassedDate = (date: Date) => new Date(this.today) > date;
 }
 
 export default new DatesService();
