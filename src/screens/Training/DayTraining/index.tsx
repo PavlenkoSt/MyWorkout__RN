eed9f004528a9b6973a5b5d7ccd0dea17357b81b
PavlenkoSt <verbatim8872@gmail.com>
2023-09-17 11:27:59 +0300

    rm mounted from day

--- src/screens/Training/DayTraining/index.tsx
+++ src/screens/Training/DayTraining/index.tsx
@@ -3,8 +3,6 @@ import {View} from 'react-native';
 import {EStyleSheet} from 'react-native-extended-stylesheet-typescript';
 import {useSelector} from 'react-redux';
 
-import Loader from '@app/components/Loader';
-import useMounted from '@app/hooks/useMounted';
 import {trainingDateSelector} from '@app/store/selectors/trainingDaySelectors';
 
 import NoTrainingYet from './NoTrainingYet';
@@ -15,13 +13,9 @@ const DayTraining: FC = () => {
 
   const [isCreation, setIsCreation] = useState(false);
 
-  const {mounted} = useMounted();
-
   return (
     <View style={styles.container}>
-      {!mounted ? (
-        <Loader />
-      ) : !trainingDay && !isCreation ? (
+      {!trainingDay && !isCreation ? (
         <NoTrainingYet onStartPlanning={() => setIsCreation(true)} />
       ) : (
         <TrainingBody isCreation={isCreation} />
