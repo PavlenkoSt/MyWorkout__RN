a059df6c786f5eb75756d2243b41d0310f1fb3a8
PavlenkoSt <verbatim8872@gmail.com>
2023-09-23 18:11:02 +0300

    react-native-get-random-values

--- index.js
+++ index.js
@@ -8,5 +8,6 @@ import {name as appName} from './app.json';
 import App from './src/App';
 
 import 'react-native-gesture-handler';
+import 'react-native-get-random-values';
 
 AppRegistry.registerComponent(appName, () => App);
