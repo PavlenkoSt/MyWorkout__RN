d9707db5488e7c635ecce6b28281d9dd1f6efafb
PavlenkoSt <verbatim8872@gmail.com>
2023-10-11 23:10:03 +0300

    fix enable autocomplete switch

--- src/store/selectors/settingsSelector.ts
+++ src/store/selectors/settingsSelector.ts
@@ -1,7 +1,7 @@
 import {RootState} from '../index';
 
 export const enableAutocompleteSelector = (state: RootState) =>
-  state.settings.exercisesForAutocomplete;
+  state.settings.enableAutocomplete;
 
 export const exercisesForAutocompleteSelector = (state: RootState) => {
   return state.settings.exercisesForAutocomplete;
