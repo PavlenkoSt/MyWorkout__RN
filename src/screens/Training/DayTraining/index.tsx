986065b7fe604d5c34514f7b53ae81ec09e5ba46
PavlenkoSt <verbatim8872@gmail.com>
2024-06-28 17:56:02 +0300

    automatically add warmup on training creation

--- src/screens/Training/DayTraining/index.tsx
+++ src/screens/Training/DayTraining/index.tsx
@@ -1,19 +1,33 @@
 import React, {FC, useState} from 'react';
 import {View} from 'react-native';
 import {EStyleSheet} from 'react-native-extended-stylesheet-typescript';
-import {useSelector} from 'react-redux';
+import {useDispatch, useSelector} from 'react-redux';
 
 import {trainingDateSelector} from '@app/store/selectors/trainingDaySelectors';
 
 import NoTrainingYet from './NoTrainingYet';
 import TrainingBody from './TrainingBody';
+import {exerciseConstructor} from '@app/utilts/exerciseConstructor';
+import {addExercise} from '@app/store/slices/trainingDaySlice';
+import {v4} from 'uuid';
 
 const DayTraining: FC = () => {
   const trainingDay = useSelector(trainingDateSelector);
 
   const [isCreation, setIsCreation] = useState(false);
 
+  const dispatch = useDispatch();
+
   const onStartPlanning = () => {
+    const exercise = exerciseConstructor.generateWarmupExerciseBody();
+    dispatch(
+      addExercise({
+        ...exercise,
+        setsDone: 0,
+        id: v4(),
+      }),
+    );
+
     setIsCreation(true);
   };
 
